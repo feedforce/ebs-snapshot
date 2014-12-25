d6702767781a0ceb8eadcd70e7150372d6ee1244
Kota Kunishima <kunishima@feedforce.jp>
2014-12-25 17:19:03 +0900

    Remove autoloaded environment

--- create_snapshot.rb
+++ create_snapshot.rb
@@ -1,10 +1,5 @@
-require 'rubygems'
 require 'aws-sdk'
 
-AWS.config(
-  access_key_id:     ENV['AWS_ACCESS_KEY'],
-  secret_access_key: ENV['AWS_SECRET_KEY'],
-  region:            ENV['AWS_REGION']
-)
+AWS.config(region: ENV['AWS_REGION'])
 
 AWS.ec2.volumes[ENV['AWS_EBS_VOLUME_ID']].create_snapshot(ENV['DESCRIPTION'])
